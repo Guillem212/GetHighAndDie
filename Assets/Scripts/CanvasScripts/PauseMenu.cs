336a0c57cac67172ae218aa2979ee3216ed3d784
Arthur <aknegtel@gmail.com>
2018-12-27 12:48:35 +0100

    Menú Pausa Completado

---
+++ Assets/Scripts/CanvasScripts/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public GameObject controlsUI;
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if(GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        // Completado
+        pauseMenuUI.SetActive(false);
+        controlsUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() {
+        // Completado
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadControls() {
+        Debug.Log("Loading Controls...");
+        pauseMenuUI.SetActive(false);
+        controlsUI.SetActive(true);
+    }
+
+    public void LoadMenu() {
+        // Completado
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Scenes/Menu");
+    }
+
+    public void QuitGame() {
+        // Completado
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    public void BackButton() {
+        pauseMenuUI.SetActive(true);
+        controlsUI.SetActive(false);
+    }
+
+}
