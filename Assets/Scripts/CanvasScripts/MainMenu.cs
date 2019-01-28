6c5ff986ff0b9101f956b2c2e15a0b8479d17f35
Arthur <aknegtel@gmail.com>
2019-01-28 22:46:24 +0100

    Música del menú

--- Assets/Scripts/CanvasScripts/MainMenu.cs
+++ Assets/Scripts/CanvasScripts/MainMenu.cs
@@ -5,14 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start() {
+        FindObjectOfType<AudioManager>().Stop("MainTheme");
+        FindObjectOfType<AudioManager>().Play("MenuTheme");
+    }
+
     public void PlayGame()
     {
         LevelChangerScript.Instance.FadeToNextLevel("World1Level1");
+        FindObjectOfType<AudioManager>().Stop("MenuTheme");
+        FindObjectOfType<AudioManager>().Play("MainTheme");
     }
 
     public void PlayTutorial()
     {
         LevelChangerScript.Instance.FadeToNextLevel("Tutorial");
+        FindObjectOfType<AudioManager>().Stop("MenuTheme");
+        FindObjectOfType<AudioManager>().Play("MainTheme");
     }
 
     public void QuitGame()
